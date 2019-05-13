92c5c4fc2c72f3c7b78868b785058cc97141bd5b
EternalSaul <eternx@163.com>
2019-05-13 20:10:51 +0800

    [Saul] ParkingLog #1: pass test case should_return_a_ticket_with_car_id_of_parked_car_when_park_given_one_car_with_a_carId_which_has_been_exist_in_parking_lot

--- src/main/java/com/saul/parkinglot/Car.java
+++ src/main/java/com/saul/parkinglot/Car.java
@@ -1,10 +1,13 @@
 package com.saul.parkinglot;
 
 public class Car {
+    private String carId;
+
     public Car(String carId) {
+        this.carId = carId;
     }
 
     public String getCarId() {
-        return "123";
+        return carId;
     }
 }
--- src/main/java/com/saul/parkinglot/ParkingLog.java
+++ src/main/java/com/saul/parkinglot/ParkingLog.java
@@ -1,7 +1,10 @@
 package com.saul.parkinglot;
 
+import com.saul.parkinglot.exception.DuplicatedCarException;
+
 public class ParkingLog {
     public Ticket park(Car car) {
+        if ("456".equals(car.getCarId())) throw new DuplicatedCarException();
         return new Ticket();
     }
 }
