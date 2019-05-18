44649987d5d261901d1a1099d9a62450608747fc
EternalSaul <eternx@163.com>
2019-05-18 15:02:45 +0800

    [Saul] Graduate Parking Boy #2: add test case should_return_a_ticket_with_car_id_and_car_be_parked_on_parkingLotA_of_parked_car_when_park_given_one_car_with_a_carId_which_has_not_been_exist_in_parking_lot_and_parking_log_and_only_b_have_enough_car_sites

--- src/test/java/com/saul/parkinglot/GraduateParkingBoyTest.java
+++ src/test/java/com/saul/parkinglot/GraduateParkingBoyTest.java
@@ -22,4 +22,17 @@ class GraduateParkingBoyTest {
         verify(parkingLogB, times(0)).park(car);
         assertEquals(ticket.getCarId(), car.getCarId());
     }
+
+    @Test
+    void should_return_a_ticket_with_car_id_and_car_be_parked_on_parkingLotA_of_parked_car_when_park_given_one_car_with_a_carId_which_has_not_been_exist_in_parking_lot_and_parking_log_and_only_b_have_enough_car_sites() {
+        GraduateParkingBoy parkingBoy = new GraduateParkingBoy();
+        ParkingLog parkingLogA = spy(new ParkingLog(0));
+        ParkingLog parkingLogB = spy(new ParkingLog(1));
+        parkingBoy.manage(parkingLogA, parkingLogB);
+        Car car = new Car("123");
+        Ticket ticket = parkingBoy.park(car);
+        verify(parkingLogA, times(0)).park(car);
+        verify(parkingLogB, times(1)).park(car);
+        assertEquals(ticket.getCarId(), car.getCarId());
+    }
 }
