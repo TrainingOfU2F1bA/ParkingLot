a9d5f064cf92b5839476f173bb8cb6062e19ad71
EternalSaul <eternx@163.com>
2019-05-18 13:22:43 +0800

    [Saul] ParkingLog #2: Graduate Parking Boy

---
+++ src/test/java/com/saul/parkinglot/GraduateParkingBoyTest.java
@@ -0,0 +1,10 @@
+package com.saul.parkinglot;
+
+import com.saul.parkinglot.exception.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraduateParkingBoyTest {
+
+}
