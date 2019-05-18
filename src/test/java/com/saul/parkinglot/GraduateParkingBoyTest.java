8fec8f51fa6383684c4d23dbeaab0aef84d36ea5
EternalSaul <eternx@163.com>
2019-05-18 15:59:01 +0800

    [Saul] Graduate Parking Boy #2: pass test case should_throw_no_enough_car_site_exception_when_park_given_one_car_with_a_carId_which_has_not_been_exist_in_parking_lot_but_car_sites_is_not_enough

--- src/test/java/com/saul/parkinglot/GraduateParkingBoyTest.java
+++ src/test/java/com/saul/parkinglot/GraduateParkingBoyTest.java
@@ -108,4 +108,11 @@ class GraduateParkingBoyTest {
         assertThrows(InvalidTicketException.class, () -> parkingLog.fetch(new Ticket("123")));
     }
 
+    @Test
+    void should_throw_no_enough_car_site_exception_when_park_given_one_car_with_a_carId_which_has_not_been_exist_in_parking_lot_but_car_sites_is_not_enough() {
+        ParkingLog parkingLog = new ParkingLog(1);
+        Car car = new Car("123");
+        parkingLog.park(car);
+        assertThrows(NoEnoughCarSiteExpection.class, () -> parkingLog.park(new Car("456")));
+    }
 }
