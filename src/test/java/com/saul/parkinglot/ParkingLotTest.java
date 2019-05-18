c66018781c548d8a73b885e95d691f8a556198f1
EternalSaul <eternx@163.com>
2019-05-18 13:32:39 +0800

    [Saul] ParkingLot #1 : add test case should_throw_no_enough_car_site_exception_when_park_given_one_car_with_a_carId_which_has_not_been_exist_in_parking_lot

--- src/test/java/com/saul/parkinglot/ParkingLotTest.java
+++ src/test/java/com/saul/parkinglot/ParkingLotTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class ParkingLotTest {
     @Test
     void should_return_a_ticket_with_car_id_of_parked_car_when_park_given_one_car_with_a_carId_which_has_not_been_exist_in_parking_lot() {
-        ParkingLog parkingLog = new ParkingLog();
+        ParkingLog parkingLog = new ParkingLog(1);
         Car car = new Car("123");
         Ticket ticket = parkingLog.park(car);
         assertEquals(ticket.getCarId(), car.getCarId());
@@ -18,45 +18,52 @@ class ParkingLotTest {
 
     @Test
     void should_throw_duplicated_car_exception_when_park_given_one_car_with_a_carId_which_has_been_exist_in_parking_lot() {
-        ParkingLog parkingLog = new ParkingLog();
+        ParkingLog parkingLog = new ParkingLog(1);
         parkingLog.park(new Car("456"));
         assertThrows(DuplicatedCarException.class, () -> parkingLog.park(new Car("456")));
     }
 
     @Test
     void should_throw_invalid_car_exception_when_park_given_one_car_without_a_carId() {
-        ParkingLog parkingLog = new ParkingLog();
+        ParkingLog parkingLog = new ParkingLog(1);
         assertThrows(InvalidCarException.class, () -> parkingLog.park(new Car(null)));
     }
 
     @Test
     void should_throw_car_does_not_exist_exception_when_park_given_no_car() {
-        ParkingLog parkingLog = new ParkingLog();
+        ParkingLog parkingLog = new ParkingLog(1);
         assertThrows(CarDoesNotExistException.class, () -> parkingLog.park(null));
     }
 
     @Test
     void should_fetch_success_when_fetch_given_one_ticket_with_a_carId_of_a_car_which_exist_in_parking_log() {
-        ParkingLog parkingLog = new ParkingLog();
+        ParkingLog parkingLog = new ParkingLog(1);
         Ticket ticket = parkingLog.park(new Car("123"));
         assertDoesNotThrow(() -> parkingLog.fetch(ticket));
     }
 
     @Test
     void should_throw_invalid_ticket_exception_when_fetch_given_one_ticket_without_a_carId() {
-        ParkingLog parkingLog = new ParkingLog();
+        ParkingLog parkingLog = new ParkingLog(1);
         assertThrows(InvalidTicketException.class, () -> parkingLog.fetch(new Ticket(null)));
     }
 
     @Test
     void should_throw_ticket_does_not_exist_exception_when_fetch_given_no_ticket() {
-        ParkingLog parkingLog = new ParkingLog();
+        ParkingLog parkingLog = new ParkingLog(1);
         assertThrows(TicketDoesNotExistException.class, () -> parkingLog.fetch(null));
     }
 
     @Test
     void should_throw_invalid_ticket_exception_when_fetch_given_one_ticket_with_a_carId_of_a_car_which_does_not_exist_in_parking_log() {
-        ParkingLog parkingLog = new ParkingLog();
+        ParkingLog parkingLog = new ParkingLog(1);
         assertThrows(InvalidTicketException.class, () -> parkingLog.fetch(new Ticket("123")));
+    }
+
+    @Test
+    void should_throw_no_enough_car_site_exception_when_park_given_one_car_with_a_carId_which_has_not_been_exist_in_parking_lot() {
+        ParkingLog parkingLog = new ParkingLog(1);
+        Car car = new Car("123");
+        assertThrows(NoEnoughCarSite.class, () -> parkingLog.park(new Car("123")));
     }
 }
