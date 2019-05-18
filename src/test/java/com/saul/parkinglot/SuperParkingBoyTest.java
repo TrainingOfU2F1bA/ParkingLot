424b47fc4da3541a3d2dc78632249d71a8d2f4c5
EternalSaul <eternx@163.com>
2019-05-18 17:40:55 +0800

    [Saul] Super Parking Boy #4: pass test case should_return_a_ticket_with_car_id_and_car_be_parked_on_parkingLotA_of_parked_car_when_park_given_one_car_with_a_carId_which_has_not_been_exist_in_parking_lot_and_parking_rate_is_same_but_remain_count_of_b_more_than_a

--- src/test/java/com/saul/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/saul/parkinglot/SuperParkingBoyTest.java
@@ -29,6 +29,22 @@ class SuperParkingBoyTest {
 
 class SuperParkingBoyAndGraduateParkingBoyCompatibilityTest extends GraduateParkingBoyTest {
 
+    @Test
+    void should_return_a_ticket_with_car_id_and_car_be_parked_on_parkingLotA_of_parked_car_when_park_given_one_car_with_a_carId_which_has_not_been_exist_in_parking_lot_and_parking_rate_is_same_but_remain_count_of_b_more_than_a() {
+        GraduateParkingBoy parkingBoy = new SuperParkingBoy();
+        ParkingLog parkingLogA = spy(new ParkingLog(10));
+        ParkingLog parkingLogB = spy(new ParkingLog(100));
+        parkingBoy.manage(parkingLogA, parkingLogB);
+
+        IntStream.range(0,1).forEach(index -> parkingLogA.park(new Car("A:"+index)));
+        IntStream.range(0,10).forEach(index -> parkingLogB.park(new Car("B:"+index)));
+
+        Car car = new Car("123");
+        parkingBoy.park(car);
+        verify(parkingLogA, times(1)).park(any(Car.class));
+        verify(parkingLogB, times(11)).park(any(Car.class));
+    }
+
     @Override
     protected GraduateParkingBoy getParkingBoy() {
         return new SuperParkingBoy();
